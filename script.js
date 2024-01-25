98f4e8ed6d1352bdbad86531bb70ff18353b81d8
Matteo Ceserani <ceserani@gmail.com>
2024-01-25 09:48:02 +0100

    Scheletro applicazione

---
+++ script.js
@@ -0,0 +1,20 @@
+let prodotti = [];
+
+function showProducts(){
+    
+}
+
+function addProduct(){
+    
+    showProducts();
+}
+
+function deleteProduct(){
+    
+    showProducts();
+}
+
+function sortProducts(){
+    
+    showProducts();
+}
